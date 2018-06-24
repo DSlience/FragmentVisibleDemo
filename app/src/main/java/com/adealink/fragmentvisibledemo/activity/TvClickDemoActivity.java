3af4752dc9ee6ccebc38064e84da22abae1643e1
dxf <duxuefu@youcheyihou.com>
2018-06-24 19:48:43 +0800

    Space的占位作用

--- app/src/main/java/com/adealink/fragmentvisibledemo/activity/TvClickDemoActivity.java
+++ app/src/main/java/com/adealink/fragmentvisibledemo/activity/TvClickDemoActivity.java
@@ -1,17 +1,22 @@
 package com.adealink.fragmentvisibledemo.activity;
 
+import android.support.v4.widget.Space;
 import android.view.View;
 
 import com.adealink.baselib.framework.BaseActivity;
 import com.adealink.baselib.utils.LogUtil;
 import com.adealink.fragmentvisibledemo.R;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
  * Created by Xuefu_Du on 2018/6/24.
  */
 public class TvClickDemoActivity extends BaseActivity {
+
+    @BindView(R.id.holder_space)
+    Space holderSpace;
 
     @Override
     protected void setUpViewAndData() {
@@ -27,6 +32,7 @@ public class TvClickDemoActivity extends BaseActivity {
     @OnClick(R.id.parent_layout)
     public void onParentClicked(View view) {
         LogUtil.d("d_slience", "onParentClicked-onParentClicked");
+        holderSpace.setVisibility(View.GONE);
     }
 
 //    @OnClick(R.id.down_tv)
